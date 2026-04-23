7d164d2fc3e87cb91b2ba22c373945af6027602d
zb-keen <zb-keen@users.noreply.github.com>
2026-04-23 13:00:40 +0000

    feat: Test Code Analyzer Skill
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/components/result/index.vue
+++ src/components/result/index.vue
@@ -3228,11 +3228,11 @@ export default {
             });
 
             // 按搜索专区给的dispOrd 从-1到大排序
-            that.tabsList = that.tabsList.sort((a, b) => a.dispOrd - b.dispOrd);
+            // that.tabsList = that.tabsList.sort((a, b) => a.dispOrd - b.dispOrd);
 
             that.tabsList.forEach((tab) => {
               if (tab.subList && tab.subList.length > 0) {
-                tab.subList.sort((a, b) => a.dispOrd - b.dispOrd);
+                // tab.subList.sort((a, b) => a.dispOrd - b.dispOrd);
               }
             });
           }
